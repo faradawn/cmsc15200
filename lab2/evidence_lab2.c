04a73e6ccdd077acfd5375b4b86204d6a04eab06
faradawn <faradawny@gmail.com>
2021-04-08 22:12:43 +0800

    lab2 - quadratic graph

--- lab2/evidence_lab2.c
+++ lab2/evidence_lab2.c
@@ -36,9 +36,9 @@ int main(int argc, char *argv[]) {
     graph(0.1, 0, 2, 1, -20, 20, -1, 20);
     printf("Graph 2: y = x^2, step = 0.5 \n");
     graph(1, 0, 0, 0.5, -3, 4, -1, 16);
-    printf("Graph 3: y = -0.3x^2 -12x - 120, step = 1 \n");
+    printf("Graph 3: y = -0.3x^2 -12x - 120, step = 2 \n");
     graph(-0.3, -12, -120, 2, -40, 0, -20, 0);
-    printf("Graph 4: y = 0.25x^2 - 10.05x + 105, stepp = 1 \n");
+    printf("Graph 4: y = 0.25x^2 - 10.05x + 105, step = 1 \n");
     graph(0.25, -10.05, 105, 1, 0, 40, 0, 20);
     return 0;
 }
