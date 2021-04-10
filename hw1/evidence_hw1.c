f2dd56a2c5788ac1679df70b9ca16ad892a6cd17
faradawn <faradawny@gmail.com>
2021-04-10 19:19:34 +0800

    hw1 - cat, fraction, buoyance

--- hw1/evidence_hw1.c
+++ hw1/evidence_hw1.c
@@ -2,7 +2,34 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("#1: fraction 2 / 3 -> 0.5  \n >> ans: %lf \n",
-            fractional_part(2,3));
+    printf("\n === testing bags_of_cat_food ===\n");
+    printf("#1: 1 cat, 200g per day, 5 days -> 1 bags \n >> %u bags\n\n",
+            bags_of_cat_food(1, 200, 5));
+    printf("#2: 5 cat, 200g per day, 5 days -> 2 bags \n >> %u bags\n\n",
+            bags_of_cat_food(5, 200, 5));
+    printf("#3: 10 cat, 180g per day, 11 days -> 2 bags \n >> %u bags\n\n",
+            bags_of_cat_food(10, 180, 11));
+    printf("#4: 0 cat, 180g per day, 11 days -> 0 bags? \n >> %u bags\n\n",
+            bags_of_cat_food(0, 180, 11));
+    
+    printf("\n === testing fractional_part ===\n");
+    printf("#1: fraction 3 / 2 -> 0.5 \n >> ans: %.1f \n\n",
+            fractional_part(3,2));
+    printf("#2: fraction 5 / 2 -> 0.5 \n >> ans: %.1f \n\n",
+            fractional_part(5,2));
+    printf("#3: fraction 5 / 3 -> 0.667 \n >> ans: %.3f \n\n",
+            fractional_part(5,3));
+    printf("#4: fraction 10 / 10 -> 0 \n >> ans: %.0f \n\n",
+            fractional_part(10, 10));
+
+    printf("\n === testing is_buoyant ===\n");
+    printf("#1: weight = 20lb, radius = 2ft -> is buoyant \n >> ans: %d \n\n",
+            is_buoyant(20,2));
+    printf("#2: weight = 2860.83lb, radius = 1.14ft -> not buoyant \n >> ans: %d \n\n",
+            is_buoyant(2860.83,1.14));
+    printf("#2: weight = 60lb, radius = 60ft -> is buoyant \n >> ans: %d \n\n",
+            is_buoyant(60,60));
+    printf("#2: weight = 0.27lb, radius = 0.039ft -> not buoyant \n >> ans: %d \n\n",
+            is_buoyant(0.27,0.039));
     return 0;
 }
--- hw1/hw1.c
+++ hw1/hw1.c
@@ -10,28 +10,11 @@ unsigned int bags_of_cat_food(unsigned int num_cats,
 
 
 double fractional_part(unsigned int numerator, unsigned int denominator) {
-    return numerator/denominator;
+    return (double)numerator/denominator - numerator/denominator;
 }
 
 int is_buoyant(double weight, double radius) {
-    double v = 4/3*M_PI*pow(radius, 3);
-    printf("%lf", v);
+    double v = 4.0/3*M_PI*pow(radius, 3);
     double F = v * 62.4;
-    return F > weight;
+    return F >= weight;
 }
-
-// int main() {
-//     printf("=== testing bags_of_cat_food ===\n");
-//     printf("#1: 5 cats, 200g per day, 5 days -> 5 bags \n >> ans: %u bags\n",
-//             bags_of_cat_food(5, 500, 5));
-    
-//     printf("=== testing fractional_part ===\n");
-//     printf("#1: fraction 2 / 3 -> 0.5  \n >> ans: %llf \n",
-//             fractional_part(2,3));
-
-//     printf("=== testing is_buoyant ===\n");
-//     printf("#1: weight = 20, radius = 5 -> is buoyant \n >> ans: %d \n",
-//             is_buoyant(20,5));
-
-//     return 1;
-// }
