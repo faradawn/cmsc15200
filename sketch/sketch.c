7a4c990670823e318ba843c2d05905a2be5a7f47
faradawn <faradawny@gmail.com>
2021-04-12 13:30:53 +0800

    sketch update

--- sketch/sketch.c
+++ sketch/sketch.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-double sequence(int n) {
-	if (n == 1){
-		printf("a1 is 1 \n");
-		return 1;
-	} else {
-		double a =  (float)1/(1+2*sequence(n-1));
-		printf("a%d is %llf \n", n, a);
-		return a;
+int is_leap_year(int year) {
+	return !(year%400) || ((year%100) && !(year%4));
+}
+
+
+int days_in_month(int x, int y) {
+	switch (x) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return 31;
+		case 2:
+			if( is_leap_year(y) )
+				return 29;
+			else 
+				return 28;
+		default:
+			if(x >= 1 && x <= 12)
+				return 30;
+			else 
+				fprintf(stderr, "std: %u", x);
+				exit(1);
 	}
 }
 
 int main(int arc, char** argv) {
-	sequence(6);
+	printf(">> %d \n", days_in_month(2, 2012));
 	return 0;
 }
+
