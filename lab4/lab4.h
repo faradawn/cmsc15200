9bb1827f54980034b0887c7dddd6df22fab8dd28
faradawn <faradawny@gmail.com>
2021-04-21 16:35:37 +0800

    lab4 - copy

--- lab4/lab4.h
+++ lab4/lab4.h
@@ -1,9 +1,11 @@
 // finds the maximum and minimum of an array of positive integers
 void find_min_max(int a[], int a_len, int* min_ptr, int* max_ptr);
 
-// returns the pointe of an allocated string of stars 
+// returns the pointer of an allocated string of stars 
 char* make_star_string(int n);
 
 // returns the pointer of an allocated histogram array
 int* make_histogram(int a[], int a_len, int* hist_len_ptr);
 
+// prints the graph of histogram
+void print_histogram_visualization(int a[], int a_len);
