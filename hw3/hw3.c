fbc558769bfbc7e8201644fcfc3fb42a78978841
faradawn <faradawny@gmail.com>
2021-04-23 00:08:51 +0800

    hw4 - second

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -16,8 +16,7 @@ int count_unique_elements(int in[], unsigned int inlen) {
 
 // compresses the given array of positive integers
 int* rle_encode(int* in, unsigned int inlen, unsigned int* outlen) {
-    // declare without malloc ?
-    // does malloc initializes to zeros ?
+    // declaring out_array
     *outlen = count_unique_elements(in, inlen)*2;
     int* out_arr = (int*)malloc(sizeof(int) * *outlen);
     if (out_arr == NULL) {
@@ -25,8 +24,8 @@ int* rle_encode(int* in, unsigned int inlen, unsigned int* outlen) {
         exit(1);
     }
 
+    // loops through the in_array
     int j = 0;
-    
     for (int i = 0; i < *outlen-1; i +=2) { 
         out_arr[i] = 1;
         while (in[j] == in[j+1]) {
@@ -35,17 +34,51 @@ int* rle_encode(int* in, unsigned int inlen, unsigned int* outlen) {
         }
         out_arr[i+1] = in[j];
         j++;
-        printf("num %d: %d %d; \n", i, out_arr[i], out_arr[i+1]);
+        printf("%d %d, ", out_arr[i], out_arr[i+1]);
     }
+
     return out_arr;
+}
+
+void rle_decode(int* in, unsigned int inlen, int** out, unsigned int* outlen) {
+    printf("in array %d \n", *out[0]);
+    printf("in array %d \n", *out[1]);
+    printf("in array %d \n", *out[2]);
+    printf("in array %d \n", *out[3]);
+    // *out = (int*)malloc(sizeof(int) * 8);
+    unsigned int i, j = 0, count = 0;
+    for (i = 0; i < inlen - 1; i += 2) {
+        j = in[i];
+        printf("j is %d \n", j);
+        while(j > 0) {
+            *out[count] = in[i+1];
+            printf("%d ", *out[count]);
+            j --;
+            count ++;
+        }
+    }
+    *outlen = count;
 
 }
 
 int main(){
-    int arr[] = {1,1,2,2,2,3,4,4};
-    unsigned int arr_len = 8;
-    count_unique_elements(arr, arr_len);
-    unsigned int len;
-    int* result_arr = rle_encode(arr, arr_len, &len);
-    free(result_arr);
+    // original array
+    int original_arr[] = {1,1,2,2,2,3,4,4};
+    unsigned int original_len = 8;
+    count_unique_elements(original_arr, original_len);
+
+    // encode 
+    unsigned int compressed_len;
+    int* compressed_arr = rle_encode(original_arr, original_len, &compressed_len);
+    printf("compressed len: %d \n", compressed_len);
+    
+    // decode 
+    int* decoded_arr = (int[8]){};
+    unsigned int decoded_len;
+    rle_decode(compressed_arr, compressed_len, &decoded_arr, &decoded_len);
+
+    // free
+    free(compressed_arr);
+
+
 }
