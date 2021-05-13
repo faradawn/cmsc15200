1a3cd3eaff635b7304906ae91d38ddf0f9c7898f
faradawn <faradawny@gmail.com>
2021-05-13 13:46:22 +0800

    binary tree 1

--- lab7/bst.c
+++ lab7/bst.c
@@ -24,33 +24,60 @@ int bst_insert(bst *t, vcard *c)
     exit(1);
   }
 
-  if(strcmp(c->cnet, t->c->cnet) == 0){
-    return 0;
-  } else if(strcmp(c->cnet, t->c->cnet) > 0){
-    t->right = bst_insert(t->right, c);
-  } else { 
-    t->left = bst_insert(t->left, c);
+  while(t){
+    if(strcmp(c->cnet, t->c->cnet) == 0){
+      return 0;
+    } else if(strcmp(c->cnet, t->c->cnet) > 0){
+      if(t->right == NULL){
+        t->right = bst_singleton(c);
+        return 1;
+      }
+      t = t->right;
+    } else if(strcmp(c->cnet, t->c->cnet) < 0){
+      if(t->left == NULL){
+        t->left = bst_singleton(c);
+        return 1;
+      }
+      t = t->left;
+    }
   }
-  
-  return t;
+  return 0;
 }
 
 unsigned int bst_num_entries(bst *t)
 {
-  fprintf(stderr,_bst_todo_format,"bst_num_entries");
-  exit(1);  
+  if(t == NULL){
+    return 0;
+  }
+  return 1 + bst_num_entries(t->left) + bst_num_entries(t->right);
+
 }
 
 unsigned int bst_height(bst *t)
 {
-  fprintf(stderr,_bst_todo_format,"bst_height");
-  exit(1);
+  // todo
+  if(t == NULL){
+    return 0;
+  }
+  return 10;
 }
 
 vcard *bst_search(bst *t, char *cnet, int *n_comparisons)
 {
-  fprintf(stderr,_bst_todo_format,"bst_search");
-  exit(1);
+  int count = 0;
+  while(t){
+    if(strcmp(cnet, t->c->cnet) == 0){
+      return t->c;
+    } else if(strcmp(cnet, t->c->cnet) > 0){
+      t = t->right;
+    } else if(strcmp(cnet, t->c->cnet) < 0){
+      t = t->left;
+    }
+    count ++;
+  }
+
+  *n_comparisons = count;
+  return NULL;
 }
 
 /* note: f is the destination of the output, e.g. the screen;
@@ -60,12 +87,34 @@ vcard *bst_search(bst *t, char *cnet, int *n_comparisons)
  */
 unsigned int bst_c(FILE *f, bst *t, char c)
 {
-  fprintf(stderr,_bst_todo_format,"bst_c");
-  exit(1);
+  if (t == NULL){
+    return 0;
+  }
+  // find the smallest qualified cnet 
+  if(c == t->c->cnet[0]){
+    if(t->left->c->cnet[0] != c){
+      // print the smallest first
+      fprintf(f, "%s \n", t->c->cnet);
+      return 1 + bst_c(f, t->right, c);
+    } 
+    // then print the current
+    fprintf(f, "%s \n", t->c->cnet);
+    return 1;
+  } else if (c < t->c->cnet[0]){
+    return bst_c(f, t->left, c);
+  } else {
+    return bst_c(f, t->right, c);
+  }
 }
 
 void bst_free(bst *t)
 {
-  fprintf(stderr,_bst_todo_format,"bst_free");
-  exit(1);
+  if(t->left != NULL){
+    bst_free(t->left);
+    free(t->left);
+  } else if (t->right != NULL){
+    bst_free(t->right);
+    free(t->right);
+  }
+  free(t);
 }
--- lab7/test.c
+++ lab7/test.c
@@ -5,6 +5,6 @@
 
 
 int main(){
-    
+    printf("%d\n",'a' < 'b');    
     
 }
